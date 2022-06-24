931c219d2521fc75a75de77cdd111571b1640259
KilviaSantos <kilviamssantos@gmail.com>
2022-06-24 15:31:04 -0300

    Revert "Exercício 1 do Livro Aprendendo a Programar"
    This reverts commit dc297cf0470607d11d1a39d75c5a47fda6364e5e.

--- Calculadora Idade Canina.py
+++
@@ -1,7 +0,0 @@
-dog_name = input("What's dog's name?")
-print(dog_name)
-dog_age = input("What's dog's age?")
-print(dog_age)
-human_age = 7 * int(dog_age)
-print('Your dog',dog_name,'is',human_age,'years old in human years')
-
