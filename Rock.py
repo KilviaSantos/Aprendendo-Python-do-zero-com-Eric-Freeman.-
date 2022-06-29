87141e634798da3176dbadecc252c4c26e4d0db5
KilviaSantos <kilviamssantos@gmail.com>
2022-06-28 23:58:48 -0300

    User-chosen input

--- Rock.py
+++ Rock.py
@@ -7,7 +7,7 @@ elif random_choice == 1:
     computer_choice = 'paper'
 else:
     computer_choice = 'scissors'
-
-print('The computer chooses', computer_choice)        
+user_choice = input('rock, paper or scissors?') 
+print('You chose', user_choice, 'and the computer chose', computer_choice)        
 
 
