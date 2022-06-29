9cff18f048a893664f80b252fe276b7413ae3677
KilviaSantos <kilviamssantos@gmail.com>
2022-06-29 00:22:28 -0300

    Evaluating Equal Choices -Tie

--- Rock.py
+++ Rock.py
@@ -1,8 +1,10 @@
 import random
 
+winner = ''
 choices = ['rock', 'paper', 'scissors']
 computer_choice = random.choice(choices)
-user_choice = input('rock, paper or scissors?') 
-print('You chose', user_choice, 'and the computer chose', computer_choice)        
+user_choice = input('rock, paper or scissors? ')       
 
+if computer_choice == user_choice:
+    winner = 'Tie'
 
