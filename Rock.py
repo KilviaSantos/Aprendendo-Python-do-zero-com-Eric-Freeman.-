7a7cc02c084c63697b81df163e93f74bccaedd9e
KilviaSantos <kilviamssantos@gmail.com>
2022-06-28 23:49:48 -0300

    Computer choice with conditional expressions.

--- Rock.py
+++ Rock.py
@@ -1,8 +1,13 @@
-#se random_choice==0, computer_choice=='rock'
-#se random_choice==1, computer_choice=='paper'
-#se random_choice==2, computer_choice=='scissors'
-
 import random
 
 random_choice = random.randint(0,2)
-print('The computer chooses', random_choice)
+if random_choice == 0:
+    computer_choice = 'rock'
+elif random_choice == 1:
+    computer_choice = 'paper'
+else:
+    computer_choice = 'scissors'
+
+print('The computer chooses', computer_choice)        
+
+
