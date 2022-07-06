5113fb084512a57e237f0a532b0917854eca8e6c
KilviaSantos <kilviamssantos@gmail.com>
2022-07-06 13:29:40 -0300

    Boolean operator - AND - winner

--- Rock.py
+++ Rock.py
@@ -8,3 +8,18 @@ user_choice = input('rock, paper or scissors? ')
 if computer_choice == user_choice:
     winner = 'Tie'
 
+elif computer_choice == 'paper' and user_choice == 'rock':
+    winner = 'Computer'
+
+elif computer_choice == 'rock' and user_choice == 'scissors' :
+    winner = 'Computer'
+
+elif computer_choice == 'scissors' and user_choice == 'paper' :
+    winner = 'Computer'   
+
+else:
+    winner = 'User'   
+
+print('The', winner, 'wins!')
+
+
