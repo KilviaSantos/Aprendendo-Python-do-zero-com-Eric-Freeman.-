c14dd94bbf3f87f2562a24625902f12e17b81032
KilviaSantos <kilviamssantos@gmail.com>
2022-07-11 10:49:27 -0300

    Put comment before the code.

--- Rock.py
+++ Rock.py
@@ -1,21 +1,24 @@
-import random
 # Importa o módulo random.
-winner = ''
-# Configura a variável winner.
+import random
 
-choices = ['rock', 'paper', 'scissors']
-computer_choice = random.choice(choices)
+# Configura a variável winner.
+winner = ''
+
 # O computador escolhe aleatoriamente pedra, papel ou tesoura através do 
 # random.choice.
+choices = ['rock', 'paper', 'scissors']
+computer_choice = random.choice(choices)
 
+# Identifica se o usuário fez uma escolha inválida e o while solicicita novamente
+# até que seja uma entrada válida. 
 user_choice = ''
 while (user_choice != 'rock' and 
         user_choice != 'paper' and
         user_choice != 'scissors') :
     user_choice = input('rock, paper or scissors? ')
-# Identifica se o usuário fez uma escolha inválida e o while solicicita novamente
-# até que seja uma entrada válida.        
 
+# Aqui é organzada a lógica, baseada na vitória do computadr para definir qual será
+# a variável winner.    
 if computer_choice == user_choice:
     winner = 'Tie'
 elif computer_choice == 'paper' and user_choice == 'rock':
@@ -26,11 +29,9 @@ elif computer_choice == 'scissors' and user_choice == 'paper' :
     winner = 'Computer'   
 else:
     winner = 'User'   
-# Aqui é organzada a lógica, baseada na vitória do computadr para definir qual será
-# a variável winner.
 
+# Anuncia o empate ou o vencedor junto com a escolha do computador.
 if winner == 'Tie':
     print('We both chose', computer_choice + ', play again.')
 else :
     print(winner, 'won. The computer chose', computer_choice + '.')
-# Anuncia o empate ou o vencedor junto com a escolha do computador.
