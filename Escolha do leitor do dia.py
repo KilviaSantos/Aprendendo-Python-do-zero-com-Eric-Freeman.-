3a9202873fdcbde3e550a13c9158e83c895beddd
KilviaSantos <kilviamssantos@gmail.com>
2022-09-07 11:28:23 -0300

    reader of the day moved to exercise folder

--- Escolha do leitor do dia.py
+++
@@ -1,9 +0,0 @@
-import random
-print ('Leitor do dia')
-lista_pessoas = ['Abraão', 'Kilvia', 'Fabíola', 'Sophia', 'Sarah']
-lista_livros = ['Eu de cabeça para baixo', 'Bíblia de adulto', 'Bíblia infantil', 'Frozen', 'O dever da esperança']
-
-pessoa = random.choice(lista_pessoas)
-livro = random.choice(lista_livros)
-
-print('O leitor de hoje é ' +  pessoa + ', que vai ler o livro: '+ livro)
